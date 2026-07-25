910638c6aca193abf0996e568305bb4be1bc59b4
siddhant-agrawal01 <siddhantagrawal001@gmail.com>
2026-07-25 19:56:27 +0530

    added the routes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import app from './app.js';
+import { startListener } from './config/listener.js';
 
+const PORT = process.env.PORT || 3000;
 
+startListener();
 
 app.listen(3000, () => {
   console.log(`[server] listening on http://localhost:3000`);
